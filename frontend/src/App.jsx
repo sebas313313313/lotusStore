6cf9e7c67ffd976aede4137ede09dddf91eae330
Tu Nombre <tu@email.com>
2025-01-27 17:34:32 -0500

    Inicialización del proyecto: Configuración inicial de frontend y backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        {/* Tu contenido irá aquí */}
+      </div>
+    </Router>
+  )
+}
+
+export default App
